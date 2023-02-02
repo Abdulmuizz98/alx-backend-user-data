cd181736fcea795ce31fff9d13d5d49a1a04ee6f
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-02-02 16:06:10 +0100

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 log info for privacy"""
 
 import re
+import logging
 from typing import List
 
 
@@ -13,3 +14,24 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         reg: str = '(?<={}=).*?(?={})'.format(field, separator)
         message = re.sub(reg, redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initializes the sub class RedactingFormatter """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format the log record with the function filter_datum """
+        record.msg = record.msg.replace(';', '; ')
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
