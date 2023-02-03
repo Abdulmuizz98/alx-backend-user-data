1a2cfd14b52e43243d144c9800477f70784b451d
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-02-03 22:01:20 +0100

    done

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """Contains the function bcrypt """
 
-from bcrypt import hashpw, gensalt, checkpw
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     """Returns a salted, hashed password which is a byte string."""
-    return hashpw(password.encode(), gensalt())
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Validate that the provided password matches the hashed password """
-    return checkpw(password.encode(), hashed_password)
+    return bcrypt.checkpw(password.encode(), hashed_password)
