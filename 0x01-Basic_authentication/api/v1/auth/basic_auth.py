6a4df78ab039a67f3ef5cf87d8f3eb8c6f9a8146
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-02-07 23:40:27 +0100

    done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,32 +8,6 @@ from api.v1.auth.auth import Auth
 
 
 class BasciAuth(Auth):
+    """ Defines the BasicAuth object
     """
-    """
-    def __init__(self):
-        """ Initializes the Auth class
-        """
-        pass
-
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Return True if path is not in excluded_paths
-        """
-        if path is None or excluded_paths in [None, []]:
-            return True
-        if path[-1] != '/':
-            path += '/'
-        if path not in excluded_paths:
-            return True
-        return False
-
-    def authorization_header(self, request=None) -> str:
-        """ Get value of request Authorization header
-        """
-        if not request or not request.headers.get('Authorization', None):
-            return None
-        return request.headers.get('Authorization')
-
-    def current_user(self, request=None) -> TypeVar('User'):
-        """ Not yet implemented Completely
-        """
-        return None
+    pass
