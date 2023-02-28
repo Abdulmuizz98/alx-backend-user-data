34c8b3b05fd450a186df99e9ae8a42b3faf92643
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-02-28 19:46:43 +0100

    done

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -78,3 +78,14 @@ class Auth:
         """
         self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate reset_token and returns it
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_uuid = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_uuid)
+            return reset_uuid
+        except NoResultFound:
+            raise ValueError('User DNE')
