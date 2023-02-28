9fc7e08632c15c0447bb6c5ff30446a611d554c8
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-02-28 18:43:31 +0100

    done

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -51,7 +51,6 @@ def logout():
     """Sessions Endpoint
     """
     session_id = request.cookies.get('session_id')
-    print(session_id)
     user = AUTH.get_user_from_session_id(session_id)
     if not user:
         abort(403)
